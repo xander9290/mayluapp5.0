68d8a470617aa121400b5b955e4fcf5e0ddd3989
Alexander <raul.alex.aguiluna@gmail.com>
2022-01-17 02:43:52 -0600

    finishing declaracion

--- src/index.js
+++ src/index.js
@@ -6,9 +6,5 @@ app.listen(app.get("port"), () => {
   console.log("Servidor iniciado en puerto ", app.get("port"));
 });
 (async () => {
-<<<<<<< HEAD
   await open(`http://localhost:${app.get("port")}`);
-=======
-await open(`http://localhost:${app.get("port")}`);
->>>>>>> 59ecced73321a9eebdb9d4830ea0ce8fc76c8592
 })();
