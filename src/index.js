00f6e762461b84bd6b2d5be14cd5ddb70d26b143
Alexander <raul.alex.aguiluna@gmail.com>
2022-01-17 02:37:32 -0600

    finishing declaracion

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import "./database";
 app.listen(app.get("port"), () => {
   console.log("Servidor iniciado en puerto ", app.get("port"));
 });
-// (async () => {
-//   await open(`http://localhost:${app.get("port")}`);
-// })();
+(async () => {
+  await open(`http://localhost:${app.get("port")}`);
+})();
