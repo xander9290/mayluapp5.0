ea56994e4966b941e0a18133fbd8b4eea4714dd5
Alexander <raul.alex.aguiluna@gmail.com>
2022-02-02 23:59:05 -0600

    algunas modificaciones

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ app.listen(app.get("port"), () => {
   console.log("Servidor iniciado en puerto ", app.get("port"));
 });
 (async () => {
+  console.log("Iniciando aplicación");
   await open(`http://localhost:${app.get("port")}`);
 })();
